e19668755aeac05a60d1aed3dbd4ee65b65cbabe
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2021-10-21 23:30:17 +0300

    feat: add saving visible-settings

--- momentum/assets/js/settings.js
+++ momentum/assets/js/settings.js
@@ -1,6 +1,7 @@
 myStorage = window.localStorage;
 
 
+
 if (!myStorage.language) {
     myStorage.setItem('language', "en");
 }
@@ -12,16 +13,42 @@ if (!myStorage.blocks) {
 }
 
 
+
+//видимость
+let visibilityBlocksAll = document.querySelectorAll(".visibility input")
+
 window.addEventListener("load", function () {
 
-    let arr = myStorage.blocks.split(",")
-    console.log(arr)
-    for (const item of arr) {
-        let invisBlock = document.getElementById(item)
-        invisBlock.checked = "true"
-
+    let arrayBlocks = myStorage.blocks.split(",")
+    for (const elem of visibilityBlocksAll) {
+        let deleteItem = document.getElementsByClassName(elem.id) //делаем все элементы невидимыми
+        deleteItem[0].classList.add("invis")
+        let str = (deleteItem[0].className).split(" ")[0]
+        if (arrayBlocks.includes(str)) { //делаем видимыми если они сохранены выбранными
+            deleteItem[0].classList.remove("invis")
+        }
+    }
+    for (const item of arrayBlocks) {
+        let invisBlockTitle = document.getElementById(item) //делаем нажатыми все сохраненные чекбоксы
+        invisBlockTitle.checked = "true";
     }
 })
+
+for (const item of visibilityBlocksAll) {
+    item.addEventListener("change", function () {
+        let deleteItem = document.getElementsByClassName(item.id)
+        deleteItem[0].classList.toggle("invis")
+        let visibibleBlocks = document.querySelectorAll(".visibility input:checked")
+        let arrayBlocks = [];
+        for (const elem of visibibleBlocks) {
+            arrayBlocks.push(elem.id)
+        }
+        myStorage.blocks = arrayBlocks
+
+    })
+}
+
+
 //язык
 let languages = document.querySelectorAll(".language input[name='language']")
 for (const item of languages) {
@@ -36,22 +63,5 @@ for (const item of bgSources) {
     item.addEventListener("change", function () {
         myStorage.photoSource = item.id
         alert(myStorage.photoSource)
-    })
-}
-//видимость
-let visibilityBlocksAll = document.querySelectorAll(".visibility input")
-for (const item of visibilityBlocksAll) {
-    item.addEventListener("change", function () {
-        let deleteItem = document.getElementsByClassName(item.id)
-        deleteItem[0].classList.toggle("invis")
-
-        let visibibleBlocks = document.querySelectorAll(".visibility input:checked")
-        let arrayBlocks = [];
-
-        for (const elem of visibibleBlocks) {
-            arrayBlocks.push(elem.id)
-        }
-        myStorage.blocks = arrayBlocks
-
     })
 }
