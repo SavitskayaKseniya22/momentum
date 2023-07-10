ed1ef3ce430bd056363c0c60551667e55f64daa2
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2023-07-10 16:45:14 +0300

    feat: update greetings logic and rearrange files

---
+++ src/modules/date/greetings/Greetings.ts
@@ -0,0 +1,38 @@
+import ActualDate from '../actualDate/ActualDate';
+import './Greetings.scss';
+
+export class Greetings {
+  constructor() {}
+
+  readStore() {
+    const storage = window.localStorage;
+    const greetingsName = storage.getItem('greetingsName');
+    return greetingsName;
+  }
+
+  writeStore(greetingsName: string) {
+    const storage = window.localStorage;
+    storage.setItem('greetingsName', greetingsName);
+  }
+
+  addListener() {
+    document
+      .querySelector('.greetings__name')
+      ?.addEventListener('change', async (event: Event) => {
+        const { target } = event;
+        if (target) {
+          this.writeStore((target as HTMLInputElement).value);
+        }
+      });
+  }
+
+  content() {
+    const timeOfDay = ActualDate.getTimeOfDay();
+    return `<div class="greetings">
+      <span class="greetings__title">Good ${timeOfDay}, </span>
+      <input type="text" placeholder="[Enter name]" class="greetings__name" value="${
+        this.readStore() || ''
+      }"  />
+    </div>`;
+  }
+}
